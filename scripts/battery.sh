ac98b703e4b36961514c57daac45b76aa180408a
byomess <hello@felipechierice.dev>
2024-03-29 16:20:19 -0300

    feat: repainting original dracula status bar

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -2,128 +2,147 @@
 # setting the locale, some users have issues with different locales, this forces the correct one
 export LC_ALL=en_US.UTF-8
 
-current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $current_dir/utils.sh
 
 linux_acpi() {
   arg=$1
   BAT=$(ls -d /sys/class/power_supply/*)
-  if [ ! -x "$(which acpi 2> /dev/null)" ];then
+  if [ ! -x "$(which acpi 2>/dev/null)" ]; then
     case "$arg" in
-      status)
-        cat $BAT/status
-        ;;
+    status)
+      cat $BAT/status
+      ;;
 
-      percent)
-        cat $BAT/capacity
-        ;;
+    percent)
+      cat $BAT/capacity
+      ;;
 
-      *)
-        ;;
+    *) ;;
     esac
   else
     case "$arg" in
-      status)
-        acpi | cut -d: -f2- | cut -d, -f1 | tr -d ' '
-        ;;
-      percent)
-        acpi | cut -d: -f2- | cut -d, -f2 | tr -d '% '
-        ;;
-      *)
-        ;;
+    status)
+      acpi | cut -d: -f2- | cut -d, -f1 | tr -d ' '
+      ;;
+    percent)
+      acpi | cut -d: -f2- | cut -d, -f2 | tr -d '% '
+      ;;
+    *) ;;
     esac
   fi
 }
 
-battery_percent()
-{
+battery_percent() {
   # Check OS
   case $(uname -s) in
-    Linux)
-      percent=$(linux_acpi percent)
-      [ -n "$percent" ] && echo " $percent"
-      ;;
+  Linux)
+    percent=$(linux_acpi percent)
+    [ -n "$percent" ] && echo "$percent"
+    ;;
 
-    Darwin)
-      echo $(pmset -g batt | grep -Eo '[0-9]?[0-9]?[0-9]%')
-      ;;
+  Darwin)
+    echo $(pmset -g batt | grep -Eo '[0-9]?[0-9]?[0-9]%')
+    ;;
 
-    FreeBSD)
-      echo $(apm | sed '8,11d' | grep life | awk '{print $4}')
-      ;;
+  FreeBSD)
+    echo $(apm | sed '8,11d' | grep life | awk '{print $4}')
+    ;;
 
-    CYGWIN*|MINGW32*|MSYS*|MINGW*)
-      # leaving empty - TODO - windows compatability
-      ;;
+  CYGWIN* | MINGW32* | MSYS* | MINGW*)
+    # leaving empty - TODO - windows compatability
+    ;;
 
-    *)
-      ;;
+  *) ;;
   esac
 }
 
-battery_status()
-{
-  # Check OS
-  case $(uname -s) in
-    Linux)
-      status=$(linux_acpi status)
-      ;;
+# # battery_status can be 'discharging', 'charging', 'charged'
+# battery_status() {
+#   # Check OS
+#   case $(uname -s) in
+#   Linux)
+#     status=$(linux_acpi status)
+#     if [ -z "$status" ]; then
+#       status=$(cat /sys/class/power_supply/BAT*/status)
+#     fi
+#     ;;
 
-    Darwin)
-      status=$(pmset -g batt | sed -n 2p | cut -d ';' -f 2 | tr -d " ")
-      ;;
+#   Darwin)
+#     status=$(pmset -g batt | sed -n 2p | cut -d ';' -f 2 | tr -d " ")
+#     ;;
 
-    FreeBSD)
-      status=$(apm | sed '8,11d' | grep Status | awk '{printf $3}')
-      ;;
+#   FreeBSD)
+#     status=$(apm | sed '8,11d' | grep Status | awk '{printf $3}')
+#     ;;
 
-    CYGWIN*|MINGW32*|MSYS*|MINGW*)
-      # leaving empty - TODO - windows compatability
-      ;;
+#   CYGWIN* | MINGW32* | MSYS* | MINGW*)
+#     # leaving empty - TODO - windows compatability
+#     ;;
 
-    *)
-      ;;
-  esac
+#   *) ;;
+#   esac
 
-  case $status in
-    discharging|Discharging)
-      echo ''
-      ;;
-    high|Full)
-      echo ''
-      ;;
-    charging|Charging)
-      echo 'AC'
-      ;;
-    *)
-      echo 'AC'
-      ;;
-  esac
-  ### Old if statements didn't work on BSD, they're probably not POSIX compliant, not sure
-  # if [ $status = 'discharging' ] || [ $status = 'Discharging' ]; then
-  # 	echo ''
-  # # elif [ $status = 'charging' ]; then # This is needed for FreeBSD AC checking support
-  # 	# echo 'AC'
-  # else
-  #  	echo 'AC'
-  # fi
-}
+#   case $status in
+#   discharging | Discharging)
+#     echo ''
+#     ;;
+#   high | Full)
+#     echo ''
+#     ;;
+#   charging | Charging)
+#     echo '󱐋'
+#     ;;
+#   *)
+#     echo '󱐋'
+#     ;;
+#   esac
+#   ### Old if statements didn't work on BSD, they're probably not POSIX compliant, not sure
+#   # if [ $status = 'discharging' ] || [ $status = 'Discharging' ]; then
+#   # 	echo ''
+#   # # elif [ $status = 'charging' ]; then # This is needed for FreeBSD AC checking support
+#   # 	# echo 'AC'
+#   # else
+#   #  	echo 'AC'
+#   # fi
+# }
 
-main()
-{
-  bat_label=$(get_tmux_option "@dracula-battery-label" "♥")
-  bat_stat=$(battery_status)
+main() {
+  ac_is_plugged=$(upower -i $(upower -e | grep AC) | grep online | awk '{print $2}' | grep -c "yes")
+  ac_plugged_icon="󱐥 "
+  ac_not_plugged_icon="󱐤 "
+
+  ac_icon=$ac_not_plugged_icon
+  if [ $ac_is_plugged -eq 1 ]; then
+    ac_icon=$ac_plugged_icon
+  fi
+
+  bat_chrg=$(upower -i $(upower -e | grep BAT) | grep state | awk '{print $2}' | grep -c "charging")
   bat_perc=$(battery_percent)
 
-  if [ -z "$bat_stat" ]; then # Test if status is empty or not
-    echo "$bat_label $bat_perc"
-  elif [ -z "$bat_perc" ]; then # In case it is a desktop with no battery percent, only AC power
-    echo "$bat_label $bat_stat"
-  else
-    echo "$bat_label $bat_stat $bat_perc"
+  bat_icon_1_4="󰂎 "
+  bat_icon_2_4="󱊡 "
+  bat_icon_3_4="󱊢 "
+  bat_icon_4_4="󱊣 "
+  bat_icon_ac_1_4="󰢟 "
+  bat_icon_ac_2_4="󱊤 "
+  bat_icon_ac_3_4="󱊥 "
+  bat_icon_ac_3_4="󱊦 "
+
+  bat_icon=$bat_icon_4_4
+
+  if [ $bat_perc -gt 75 ]; then
+    bat_icon=$bat_icon_4_4
+  elif [ $bat_perc -gt 50 ]; then
+    bat_icon=$bat_icon_3_4
+  elif [ $bat_perc -gt 25 ]; then
+    bat_icon=$bat_icon_2_4
+  elif [ $bat_perc -gt 10 ]; then
+    bat_icon=$bat_icon_1_4
   fi
+
+  echo "$ac_icon$bat_icon$bat_perc"
 }
 
 #run main driver program
 main
-
--- scripts/dracula.sh
+++ scripts/dracula.sh
@@ -2,11 +2,10 @@
 # setting the locale, some users have issues with different locales, this forces the correct one
 export LC_ALL=en_US.UTF-8
 
-current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $current_dir/utils.sh
 
-main()
-{
+main() {
   # set configuration option variables
   show_kubernetes_context_label=$(get_tmux_option "@dracula-kubernetes-context-label" "")
   eks_hide_arn=$(get_tmux_option "@dracula-kubernetes-eks-hide-arn" false)
@@ -31,36 +30,50 @@ main()
   show_synchronize_panes_label=$(get_tmux_option "@dracula-synchronize-panes-label" "Sync")
   time_format=$(get_tmux_option "@dracula-time-format" "")
   show_ssh_session_port=$(get_tmux_option "@dracula-show-ssh-session-port" false)
-  IFS=' ' read -r -a plugins <<< $(get_tmux_option "@dracula-plugins" "battery network weather")
+  IFS=' ' read -r -a plugins <<<$(get_tmux_option "@dracula-plugins" "battery network weather")
   show_empty_plugins=$(get_tmux_option "@dracula-show-empty-plugins" true)
 
-  # Dracula Color Pallette
-  white='#f8f8f2'
-  gray='#44475a'
-  dark_gray='#282a36'
-  light_purple='#bd93f9'
-  dark_purple='#6272a4'
-  cyan='#8be9fd'
-  green='#50fa7b'
-  orange='#ffb86c'
-  red='#ff5555'
-  pink='#ff79c6'
-  yellow='#f1fa8c'
+  # black = '#18274f'
+  # red = '#f03a53'
+  # green = '#91ef2d'
+  # yellow = '#ffe940'
+  # blue = '#1f5db9'
+  # magenta = '#da56cf'
+  # cyan = '#5ad3f4'
+
+  white='#f7f1e3'
+  gray='#4a4e69'
+  dark_gray='#162442'
+  light_purple='#9667e0'
+  dark_purple='#560bad'
+  cyan='#00b4d8'
+  green='#70e000'
+  orange='#FB5607'
+  red='#ef233c'
+  pink='#FF006E'
+  yellow='#ffbe0b'
+
 
   # Handle left icon configuration
   case $show_left_icon in
-    smiley)
-      left_icon="☺";;
-    session)
-      left_icon="#S";;
-    window)
-      left_icon="#W";;
-    hostname)
-      left_icon="#H";;
-    shortname)
-      left_icon="#h";;
-    *)
-      left_icon=$show_left_icon;;
+  smiley)
+    left_icon="☺"
+    ;;
+  session)
+    left_icon="#S"
+    ;;
+  window)
+    left_icon="#W"
+    ;;
+  hostname)
+    left_icon="#H"
+    ;;
+  shortname)
+    left_icon="#h"
+    ;;
+  *)
+    left_icon=$show_left_icon
+    ;;
   esac
 
   # Handle left icon padding
@@ -79,20 +92,24 @@ main()
   # Set timezone unless hidden by configuration
   if [[ -z "$timezone" ]]; then
     case $show_timezone in
-      false)
-        timezone="";;
-      true)
-        timezone="#(date +%Z)";;
+    false)
+      timezone=""
+      ;;
+    true)
+      timezone="#(date +%Z)"
+      ;;
     esac
   fi
 
   case $show_flags in
-    false)
-      flags=""
-      current_flags="";;
-    true)
-      flags="#{?window_flags,#[fg=${dark_purple}]#{window_flags},}"
-      current_flags="#{?window_flags,#[fg=${light_purple}]#{window_flags},}"
+  false)
+    flags=""
+    current_flags=""
+    ;;
+  true)
+    flags="#{?window_flags,#[fg=${dark_purple}]#{window_flags},}"
+    current_flags="#{?window_flags,#[fg=${light_purple}]#{window_flags},}"
+    ;;
   esac
 
   # sets refresh interval to every 5 seconds
@@ -118,15 +135,15 @@ main()
   tmux set-option -g pane-border-style "fg=${gray}"
 
   # message styling
-  tmux set-option -g message-style "bg=${gray},fg=${white}"
+  tmux set-option -g message-style "bg=${dark_gray},fg=${white}"
 
   # status bar
-  tmux set-option -g status-style "bg=${gray},fg=${white}"
+  tmux set-option -g status-style "bg=${dark_gray},fg=${white}"
 
   # Status left
   if $show_powerline; then
-    tmux set-option -g status-left "#[bg=${green},fg=${dark_gray}]#{?client_prefix,#[bg=${yellow}],} ${left_icon} #[fg=${green},bg=${gray}]#{?client_prefix,#[fg=${yellow}],}${left_sep}"
-    powerbg=${gray}
+    tmux set-option -g status-left "#[bg=${cyan},fg=${dark_gray}]#{?client_prefix,#[bg=${yellow}],} ${left_icon} #[fg=${cyan},bg=${dark_gray}]#{?client_prefix,#[fg=${yellow}],}${left_sep}"
+    powerbg=${dark_gray}
   else
     tmux set-option -g status-left "#[bg=${green},fg=${dark_gray}]#{?client_prefix,#[bg=${yellow}],} ${left_icon}"
   fi
@@ -136,7 +153,7 @@ main()
 
   for plugin in "${plugins[@]}"; do
 
-    if case $plugin in custom:*) true;; *) false;; esac; then
+    if case $plugin in custom:*) true ;; *) false ;; esac then
       script=${plugin#"custom:"}
       if [[ -x "${current_dir}/${script}" ]]; then
         IFS=' ' read -r -a colors <<<$(get_tmux_option "@dracula-custom-plugin-colors" "cyan dark_gray")
@@ -148,59 +165,59 @@ main()
       fi
 
     elif [ $plugin = "cwd" ]; then
-      IFS=' ' read -r -a colors  <<< $(get_tmux_option "@dracula-cwd-colors" "dark_gray white")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@dracula-cwd-colors" "dark_gray white")
       tmux set-option -g status-right-length 250
       script="#($current_dir/cwd.sh)"
 
     elif [ $plugin = "fossil" ]; then
-      IFS=' ' read -r -a colors  <<< $(get_tmux_option "@dracula-fossil-colors" "green dark_gray")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@dracula-fossil-colors" "green dark_gray")
       tmux set-option -g status-right-length 250
       script="#($current_dir/fossil.sh)"
 
     elif [ $plugin = "git" ]; then
-      IFS=' ' read -r -a colors  <<< $(get_tmux_option "@dracula-git-colors" "green dark_gray")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@dracula-git-colors" "green dark_gray")
       tmux set-option -g status-right-length 250
       script="#($current_dir/git.sh)"
 
     elif [ $plugin = "hg" ]; then
-      IFS=' ' read -r -a colors  <<< $(get_tmux_option "@dracula-hg-colors" "green dark_gray")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@dracula-hg-colors" "green dark_gray")
       tmux set-option -g status-right-length 250
       script="#($current_dir/hg.sh)"
 
     elif [ $plugin = "battery" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@dracula-battery-colors" "pink dark_gray")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@dracula-battery-colors" "pink dark_gray")
       script="#($current_dir/battery.sh)"
 
     elif [ $plugin = "gpu-usage" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@dracula-gpu-usage-colors" "pink dark_gray")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@dracula-gpu-usage-colors" "pink dark_gray")
       script="#($current_dir/gpu_usage.sh)"
 
     elif [ $plugin = "gpu-ram-usage" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@dracula-gpu-ram-usage-colors" "cyan dark_gray")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@dracula-gpu-ram-usage-colors" "cyan dark_gray")
       script="#($current_dir/gpu_ram_info.sh)"
 
     elif [ $plugin = "gpu-power-draw" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@dracula-gpu-power-draw-colors" "green dark_gray")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@dracula-gpu-power-draw-colors" "green dark_gray")
       script="#($current_dir/gpu_power.sh)"
 
     elif [ $plugin = "cpu-usage" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@dracula-cpu-usage-colors" "orange dark_gray")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@dracula-cpu-usage-colors" "orange dark_gray")
       script="#($current_dir/cpu_info.sh)"
 
     elif [ $plugin = "ram-usage" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@dracula-ram-usage-colors" "cyan dark_gray")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@dracula-ram-usage-colors" "cyan dark_gray")
       script="#($current_dir/ram_info.sh)"
 
     elif [ $plugin = "tmux-ram-usage" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@dracula-tmux-ram-usage-colors" "cyan dark_gray")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@dracula-tmux-ram-usage-colors" "cyan dark_gray")
       script="#($current_dir/tmux_ram_info.sh)"
 
     elif [ $plugin = "network" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@dracula-network-colors" "cyan dark_gray")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@dracula-network-colors" "cyan dark_gray")
       script="#($current_dir/network.sh)"
 
     elif [ $plugin = "network-bandwidth" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@dracula-network-bandwidth-colors" "cyan dark_gray")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@dracula-network-bandwidth-colors" "cyan dark_gray")
       tmux set-option -g status-right-length 250
       script="#($current_dir/network_bandwidth.sh)"
 
@@ -237,30 +254,37 @@ main()
       script="#($current_dir/continuum.sh)"
 
     elif [ $plugin = "weather" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@dracula-weather-colors" "orange dark_gray")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@dracula-weather-colors" "orange dark_gray")
       script="#($current_dir/weather_wrapper.sh $show_fahrenheit $show_location '$fixed_location')"
 
     elif [ $plugin = "time" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@dracula-time-colors" "dark_purple white")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@dracula-time-colors" "dark_purple white")
       if [ -n "$time_format" ]; then
         script=${time_format}
       else
-        if $show_day_month && $show_military ; then # military time and dd/mm
-          script="%a %d/%m %R ${timezone} "
+        _timezone=""
+        [ -n "$timezone" ] && _timezone=" $timezone"
+
+        clock_icons=("󱑋" "󱑌" "󱑍" "󱑎" "󱑏" "󱑐" "󱑑" "󱑒" "󱑓" "󱑔" "󱑕" "󱑖")
+        clock_icon=${clock_icons[$(date +%H) | bc]}
+        clock_icon=" $clock_icon"
+
+        if $show_day_month && $show_military; then # military time and dd/mm
+          script="%a %d/%m %R${_timezone}$clock_icon"
         elif $show_military; then # only military time
-          script="%a %m/%d %R ${timezone} "
+          script="%a %m/%d %R${_timezone}$clock_icon"
         elif $show_day_month; then # only dd/mm
-          script="%a %d/%m %I:%M %p ${timezone} "
+          script="%a %d/%m %I:%M %p${_timezone}$clock_icon"
         else
-          script="%a %m/%d %I:%M %p ${timezone} "
+          script="%a %m/%d %I:%M %p${_timezone}$clock_icon"
         fi
       fi
     elif [ $plugin = "synchronize-panes" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@dracula-synchronize-panes-colors" "cyan dark_gray")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@dracula-synchronize-panes-colors" "cyan dark_gray")
       script="#($current_dir/synchronize_panes.sh $show_synchronize_panes_label)"
 
     elif [ $plugin = "ssh-session" ]; then
-      IFS=' ' read -r -a colors <<< $(get_tmux_option "@dracula-ssh-session-colors" "green dark_gray")
+      IFS=' ' read -r -a colors <<<$(get_tmux_option "@dracula-ssh-session-colors" "green dark_gray")
       script="#($current_dir/ssh_session.sh $show_ssh_session_port)"
 
     else
@@ -285,12 +309,12 @@ main()
 
   # Window option
   if $show_powerline; then
-    tmux set-window-option -g window-status-current-format "#[fg=${gray},bg=${dark_purple}]${left_sep}#[fg=${white},bg=${dark_purple}] #I #W${current_flags} #[fg=${dark_purple},bg=${gray}]${left_sep}"
+    tmux set-window-option -g window-status-current-format "#[fg=${dark_gray},bg=${dark_purple}]${left_sep}#[fg=${white},bg=${dark_purple}] #I #W${current_flags} #[fg=${dark_purple},bg=${dark_gray}]${left_sep}"
   else
     tmux set-window-option -g window-status-current-format "#[fg=${white},bg=${dark_purple}] #I #W${current_flags} "
   fi
 
-  tmux set-window-option -g window-status-format "#[fg=${white}]#[bg=${gray}] #I #W${flags}"
+  tmux set-window-option -g window-status-format "#[fg=${white}]#[bg=${dark_gray}] #I #W${flags}"
   tmux set-window-option -g window-status-activity-style "bold"
   tmux set-window-option -g window-status-bell-style "bold"
 }
